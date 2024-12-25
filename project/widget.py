47d15e13d23a6c4029d9a0f7846a5ad7d1452104
HakanYadigar <hakanyadigar03@gmail.com>
2024-12-25 11:47:58 +0300

    Update widget.py

--- project/widget.py
+++ project/widget.py
@@ -3,6 +3,7 @@ import cv2
 from PySide6.QtWidgets import QApplication, QWidget, QFileDialog
 from ui_form import Ui_Widget
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Widget(QWidget):
     def __init__(self, parent=None):
@@ -27,6 +28,8 @@ class Widget(QWidget):
         self.ui.horizontalSlider_2.valueChanged.connect(self.apply_filter_2)
         self.ui.pushButton_2.clicked.connect(self.apply_edges)
         self.ui.horizontalSlider_3.valueChanged.connect(self.apply_blur)
+        self.ui.pushButton_3.clicked.connect(self.apply_circle)
+
 
     def open_image(self):
         file_name, _ = QFileDialog.getOpenFileName(
@@ -38,8 +41,8 @@ class Widget(QWidget):
         if file_name:
             self.original_image = cv2.imread(file_name)
             if self.original_image is not None:  # Görüntü yüklendiyse
-                self.current_image = self.original_image.copy()  # Geçici görüntüyü ayarla
                 cv2.imwrite('sonuc.png', self.original_image)
+                self.current_image=cv2.imread('sonuc.png')
                 self.ui.widget_2.setStyleSheet("border-image: url(sonuc.png);")
             else:
                 print("Hata: Resim yüklenemedi. Dosya bozuk olabilir.")
@@ -49,7 +52,7 @@ class Widget(QWidget):
     def apply_filter_1(self):
         if self.current_image is None:
             return
-
+        # original function
         min_value = self.ui.horizontalSlider.value()
 
         # Original image üzerinden işlem yap!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -65,14 +68,13 @@ class Widget(QWidget):
             return
 
         max_value = self.ui.horizontalSlider_2.value()
-
-        # Original image üzerinden işlem yap !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         gray = cv2.cvtColor(self.original_image, cv2.COLOR_BGR2GRAY)
         _, second_thresh = cv2.threshold(gray, max_value, 255, cv2.THRESH_BINARY)
         self.current_image = cv2.cvtColor(second_thresh, cv2.COLOR_GRAY2BGR)
 
         cv2.imwrite('sonuc.png', self.current_image)
         self.ui.widget_2.setStyleSheet("border-image: url(sonuc.png);")
+
 
     def apply_blur(self):
         if self.current_image is None:
@@ -81,8 +83,10 @@ class Widget(QWidget):
         blur_value = self.ui.horizontalSlider_3.value()
         if blur_value % 2 == 0:  # GaussianBlur kernel boyutu tek olmalı
             blur_value += 1
-
-        # Current image üzerinden işlem yap
+        self.apply_filter_1()
+        self.apply_filter_2()
+        self.current_image=cv2.imread('sonuc.png')
+        # Original image üzerinden işlem yap
         blurred_image = cv2.GaussianBlur(self.current_image, (blur_value, blur_value), 0)
         self.current_image = blurred_image
 
@@ -118,6 +122,37 @@ class Widget(QWidget):
 
         cv2.imwrite('sonuc.png', self.original_image)
         self.ui.widget_2.setStyleSheet("border-image: url(sonuc.png);")
+
+
+    def apply_circle(self):
+        # Convert to grayscale.
+        gray = cv2.cvtColor(self.original_image, cv2.COLOR_BGR2GRAY)
+
+        # Blur using 3 * 3 kernel.
+        gray_blurred = cv2.blur(gray, (3, 3))
+
+        # Apply Hough transform on the blurred image.
+        detected_circles = cv2.HoughCircles(gray_blurred,
+                           cv2.HOUGH_GRADIENT, 1, 20, param1 = 50,
+                       param2 = 30, minRadius = 1, maxRadius = 40)
+
+        # Draw circles that are detected.
+        if detected_circles is not None:
+
+            # Convert the circle parameters a, b and r to integers.
+            detected_circles = np.uint16(np.around(detected_circles))
+
+            for pt in detected_circles[0, :]:
+                a, b, r = pt[0], pt[1], pt[2]
+
+                # Draw the circumference of the circle.
+                cv2.circle(self.current_image, (a, b), r, (0, 0, 255), 2)
+
+                # Draw a small circle (of radius 1) to show the center.
+                cv2.circle(self.current_image, (a, b), 1, (255, 0, 0), 3)
+                cv2.imshow("Detected Circle", self.current_image)
+                cv2.waitKey(0)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
